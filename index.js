a69794361af92cef73d14343de9085262cfcefcf
NifatHossain <nifathossain1480@gmail.com>
2024-05-21 22:02:39 +0600

    Added user validation, single user info show in js file

--- index.js
+++ index.js
@@ -24,8 +24,40 @@ const showAllUser=async()=>{
     const data= await response.json();
     console.log(data)
 }
-const showSingleUser=async(email)=>{
-    const response = await fetch(`http://localhost:3000/allusers/${email}`);
+
+const showSingleUser=async()=>{
+    // console.log(email)
+    const response1 = await fetch(`http://localhost:3000/currentuser`);
+    const data1= await response1.json();
+    console.log(data1[0].email)
+    const currentEmail=data1[0].email;
+    const response = await fetch(`http://localhost:3000/allusers/${currentEmail}`);
     const data= await response.json();
     console.log(data)
+    // window.location.href = 'userData.html';
+    const userData= document.getElementById('userInfo');
+    userData.innerHTML=`
+        <h1 class="text-3xl font-semibold">Name: ${data[0].fName}</h1>
+    `
+
+}
+const validateUser=async(email,pass)=>{
+    const response = await fetch(`http://localhost:3000/allusers/${email}`);
+    const data= await response.json();
+    const info= {email}
+    if(data.length>0 && data[0].password==pass){
+        fetch('http://localhost:3000/addcurrentuser', {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(info)
+        })
+        .then(res=>{
+            console.log('response: ', res)
+        })
+        
+    }
+    else{
+        alert('Wrong input')
+    }
+
 }
